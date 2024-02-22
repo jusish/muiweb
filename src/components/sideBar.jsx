0d29de32c5cf8d65649e4ad22d701ff2bbe0e4c0
jusish <ishimwejustin67@gmail.com>
2024-02-22 09:12:50 +0200

    added the side bar navigation

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -1,7 +1,105 @@
-import {Box} from "@mui/material"
-
+import * as React from "react";
+import ListSubheader from "@mui/material/ListSubheader";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import DraftsIcon from "@mui/icons-material/Drafts";
+import SendIcon from "@mui/icons-material/Send";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import StarBorder from "@mui/icons-material/StarBorder";
+import Box from "@mui/material/Box";
+import Switch from "@mui/material/Switch";
+import ListItem from "@mui/material/ListItem";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
 const SideBar = () => {
-  return <Box bgcolor={"blue"}>Side bar</Box>;
+  const [open, setOpen] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  const [checked, setChecked] = React.useState(["mode"]);
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+  return (
+    <Box flexGrow={0.5}>
+      <List
+        sx={{
+          width: "100%",
+          maxWidth: 360,
+          bgcolor: "background.paper",
+          marginTop: 5,
+        }}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+        subheader={<ListSubheader>Home</ListSubheader>}
+      >
+        <ListItemButton>
+          <ListItemIcon>
+            <SendIcon />
+          </ListItemIcon>
+          <ListItemText primary="Sent mail" />
+        </ListItemButton>
+        <ListItemButton>
+          <ListItemIcon>
+            <DraftsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Drafts" />
+        </ListItemButton>
+        <ListItemButton onClick={handleClick}>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText primary="Inbox" />
+          {open ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItemButton sx={{ pl: 4 }}>
+              <ListItemIcon>
+                <StarBorder />
+              </ListItemIcon>
+              <ListItemText primary="Starred" />
+            </ListItemButton>
+          </List>
+        </Collapse>
+      </List>
+      <List
+        sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+        subheader={<ListSubheader>Settings</ListSubheader>}
+      >
+        <ListItem>
+          <ListItemIcon>
+            <Brightness4Icon />
+          </ListItemIcon>
+          <ListItemText id="switch-list-label-mode" primary="Dark-Mode" />
+          <Switch
+            edge="end"
+            onChange={handleToggle("mode")}
+            checked={checked.indexOf("mode") !== -1}
+            inputProps={{
+              "aria-labelledby": "switch-list-label-mode",
+            }}
+          />
+        </ListItem>
+      </List>
+    </Box>
+  );
 };
 
 export default SideBar;
