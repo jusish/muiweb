0ce5df302db085593478b62a342e9f5138814f57
jusish <ishimwejustin67@gmail.com>
2024-02-22 09:09:53 +0200

    changed the layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import SideBar from "./components/sideBar";
 import RightBar from "./components/rightBar";
 import Feed from "./components/Feed";
 import { Box, Stack } from "@mui/material";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
     <Box>
-      <Stack justifyContent={"space-between"} spacing={2} direction={"row"}>
-        <RightBar />
-        <Feed />
+      <Navbar />
+      <Stack justifyContent={"space-between"} spacing={0.5} direction={"row"}>
         <SideBar />
+        <Feed />
+        <RightBar />
       </Stack>
     </Box>
   );
