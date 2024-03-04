73c216e3c97e3031b3f84f36b424e564bb729f7f
jusish <ishimwejustin67@gmail.com>
2024-03-04 11:13:30 +0200

    added the dark mode configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,47 @@
+import * as React from "react";
 import SideBar from "./components/sideBar";
 import RightBar from "./components/rightBar";
 import Feed from "./components/Feed";
 import { Box, Stack } from "@mui/material";
 import Navbar from "./components/NavBar";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-
-const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
+import ColorModeContext from "./components/ColorModeContext";
 
 function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const [mode, setMode] = React.useState(prefersDarkMode ? "dark" : "light");
+
+  const handleModeChange = () => {
+    setMode((prevMode) => (prevMode === "dark" ? "light" : "dark"));
+  };
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: mode,
+        },
+      }),
+    [mode]
+  );
+
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Box sx={{ margin: 0 }}>
-        <Navbar />
-        <Stack justifyContent={"space-center"} direction={"row"}>
-          <SideBar />
-          <Feed />
-          <RightBar />
-        </Stack>
-      </Box>
-    </ThemeProvider>
+    <ColorModeContext.Provider value={{ toggleColorMode: handleModeChange }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box sx={{ margin: 0 }}>
+          <Navbar />
+          <Stack justifyContent={"space-center"} direction={"row"}>
+            <SideBar />
+            <Feed />
+            <RightBar />
+          </Stack>
+        </Box>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
