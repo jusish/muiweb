87feb33cc13ef048da9ba6855cfe832a3b0f5d36
jusish <ishimwejustin67@gmail.com>
2024-03-04 09:15:43 +0200

    dark mode configuration

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,28 @@ import RightBar from "./components/rightBar";
 import Feed from "./components/Feed";
 import { Box, Stack } from "@mui/material";
 import Navbar from "./components/NavBar";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
   return (
-    <Box sx={{margin:0}}>
-      <Navbar />
-      <Stack justifyContent={"space-center"} direction={"row"}>
-        <SideBar />
-        <Feed />
-        <RightBar />
-      </Stack>
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Box sx={{ margin: 0 }}>
+        <Navbar />
+        <Stack justifyContent={"space-center"} direction={"row"}>
+          <SideBar />
+          <Feed />
+          <RightBar />
+        </Stack>
+      </Box>
+    </ThemeProvider>
   );
 }
 
