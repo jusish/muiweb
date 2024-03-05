115ea55c70142d5cb35da7f568e7cc99ef48cf18
jusish <ishimwejustin67@gmail.com>
2024-03-05 08:33:14 +0200

    side bar navigation modified

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -5,27 +5,20 @@ import List from "@mui/material/List";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import Collapse from "@mui/material/Collapse";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import DraftsIcon from "@mui/icons-material/Drafts";
-import SendIcon from "@mui/icons-material/Send";
-import ExpandLess from "@mui/icons-material/ExpandLess";
-import ExpandMore from "@mui/icons-material/ExpandMore";
-import StarBorder from "@mui/icons-material/StarBorder";
 import Box from "@mui/material/Box";
-import Switch from "@mui/material/Switch";
 import ListItem from "@mui/material/ListItem";
-import Brightness4Icon from "@mui/icons-material/Brightness4";
-const SideBar = ({ handleModeChange }) => {
+import { Home } from "@mui/icons-material";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+const SideBar = () => {
   const [open, setOpen] = React.useState(true);
 
   const handleClick = () => {
     setOpen(!open);
   };
 
-  const toggleColorMode = () => () => {
-    handleModeChange();
-  };
   return (
     <Box flexGrow={1} position={"sticky"}>
       <List
@@ -40,9 +33,9 @@ const SideBar = ({ handleModeChange }) => {
       >
         <ListItemButton sx={{ borderRadius: 20 }}>
           <ListItemIcon>
-            <SendIcon />
+            <Home />
           </ListItemIcon>
-          <ListItemText primary="Sent mail" />
+          <ListItemText primary="Home" />
         </ListItemButton>
         <ListItemButton sx={{ borderRadius: 20 }}>
           <ListItemIcon>
@@ -55,35 +48,22 @@ const SideBar = ({ handleModeChange }) => {
             <InboxIcon />
           </ListItemIcon>
           <ListItemText primary="Inbox" />
-          {open ? <ExpandLess /> : <ExpandMore />}
         </ListItemButton>
-        <Collapse in={open} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            <ListItemButton sx={{ pl: 4, borderRadius: 20 }}>
-              <ListItemIcon>
-                <StarBorder />
-              </ListItemIcon>
-              <ListItemText primary="Starred" />
-            </ListItemButton>
-          </List>
-        </Collapse>
       </List>
       <List
         sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
         subheader={<ListSubheader>Settings</ListSubheader>}
       >
         <ListItem>
-          <ListItemIcon>
-            <Brightness4Icon />
-          </ListItemIcon>
-          <ListItemText id="switch-list-label-mode" primary="Dark" />
-          <Switch
-            edge="end"
-            onChange={toggleColorMode}
-            inputProps={{
-              "aria-labelledby": "switch-list-label-mode",
-            }}
-          />
+          <ListItemButton sx={{ borderRadius: 20 }}>
+            <ListItemAvatar>
+              <Avatar
+                alt="Cindy Baker"
+                src="/public/triangle_background-9df4fa2e10f0e294779511e99083c2bc.jpg"
+              />
+            </ListItemAvatar>
+            <ListItemText id="profile-list-label-mode" primary="Profile" />
+          </ListItemButton>
         </ListItem>
       </List>
     </Box>
